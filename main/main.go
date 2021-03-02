ac9aea940b1c2ec48ad9ff9c5ccddc7a24cd9af1
Ado <adoontheway@gmail.com>
2021-03-02 13:23:35 +0800

    day2 finished

--- main/main.go
+++ main/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"merpc"
-	"merpc/codec"
+	_ "merpc/codec"
 	"net"
+	"sync"
 	"time"
 )
 
@@ -21,25 +21,29 @@ func startServer(addr chan string) {
 }
 
 func main() {
+	log.SetFlags(0)
 	addr := make(chan string)
 	go startServer(addr)
 
-	conn, _ := net.Dial("tcp", <-addr)
+	client, _ := merpc.Dial("tcp", <-addr)
 	defer func() {
-		_ = conn.Close()
+		_ = client.Close()
 	}()
 	time.Sleep(time.Second)
-	_ = json.NewEncoder(conn).Encode(merpc.DefaultOption)
-	cc := codec.NewGobCodec(conn)
+	var wg sync.WaitGroup
+
 	for i := 0; i < 5; i++ {
-		h := &codec.Header{
-			ServiceMethod: "Foo.Sum",
-			Seq:           uint64(i),
-		}
-		_ = cc.Write(h, fmt.Sprintf("merpc req %d", h.Seq))
-		_ = cc.ReadHeader(h)
-		var reply string
-		_ = cc.ReadBody(&reply)
-		log.Println("reply:", reply)
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := fmt.Sprintf("merpc req %d", i)
+			var reply string
+			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error - ", err)
+			}
+			log.Println("reply:", reply)
+		}(i)
+
 	}
+	wg.Wait()
 }
